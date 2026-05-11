ee48b9d345e0ad3d34c63dcb2e961db2fb07b119
alissonlima22 <alisson.lima2@estudan.ifms.edu.br>
2026-05-10 23:41:25 -0300

    Telas arrumadas com os icones, deu b.o no meu git, mas consegui arrumar por isso a demora pra dar o utimo git

---
+++ src/main/java/com/mycompany/sistema_adelima/Sistema_AdeLima.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_adelima;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Sistema_AdeLima {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
